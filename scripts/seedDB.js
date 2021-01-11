ef39b90fdc392eeed9a40ee94e5d9291fcfffee6
Tedwar52 <67608987+tedwar52@users.noreply.github.com>
2021-01-10 20:41:29 -0500

    created some hard coded seeds for db

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,5 +3,34 @@ const db = require("../models");
 
 //This file empties the Favs collection and inserts seeds below
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favorites")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/favorites");
 
+const stuffSeeds = [
+    {
+        placeTitle: "The Eiffel Tower"
+    },
+    {
+        placeTitle: "The Louvre Museum"
+    },
+    {
+        placeTitle: "Pullman Pairs Tour Eiffel"
+    },
+    {
+        placeTitle: "Epicure"
+    },
+    {
+        placeTitle: "Four Seasons Hotel George V, Paris"
+    }
+]
+
+db.Fav
+    .remove({})
+    .then(() => db.Fav.collection.insertMany(stuffSeeds))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0)
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1)
+    })
